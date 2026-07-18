55a7f75cb98b027a429231bf53091c966210eb3d
ishiishikou <kou.ishi@gmail.com>
2026-07-18 19:23:01 +0900

    Handle background model download events

---
+++ Pribye/App/PribyeAppDelegate.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+@MainActor
+final class PribyeAppDelegate: NSObject, UIApplicationDelegate {
+  func application(
+    _ application: UIApplication,
+    handleEventsForBackgroundURLSession identifier: String,
+    completionHandler: @escaping () -> Void
+  ) {
+    GemmaModelDownloadManager.shared.setBackgroundCompletionHandler(
+      completionHandler,
+      for: identifier
+    )
+  }
+}
